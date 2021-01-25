f647bce00db0b36223071d2da1c6b2b3ae500cc5
zhixiong <lxsbw@outlook.com>
2021-01-25 12:33:55 +0800

    update package version

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -12,6 +12,10 @@ export const orm: ConnectionOptions = {
   logging: true
 };
 
+// 这个配置在local是占位，不起作用
+// config.cluster是 egg-cluster 的配置，midway 在 dev 时已经不再使用 egg-cluster，所以不会生效。
+// 可以在 dev 时增加端口解决 "dev": "midway-bin dev --ts --port=6001"
+// 部署时，如果使用 egg-scripts，将会继续生效。如果使用 bootstrap 方式，可以通过 configuire({port: 6001}) 参数解决。
 export const cluster = {
   listen: {
     path: '',
